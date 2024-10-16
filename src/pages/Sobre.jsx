458ebd37e413d7f09beb539ce1f4d2b2de7db13c
Milena Mafra <160766067+milenamafra96@users.noreply.github.com>
2024-10-16 12:48:50 -0300

    alteracao

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -5,7 +5,7 @@ function Sobre() {
           <div> 
                   {/* O HTML vai dentro desta <div></div> */}
             <h1>Este espaço é para fazer o login</h1>
-            <h3>Sobre o Projeto</h3>
+            <h3>Sobre oS Projeto</h3>
             <p>
             O Projeto Noel nasceu do desejo de proporcionar um Natal mais feliz para crianças em situação de vulnerabilidade. Nossa missão é conectar escolas e padrinhos dispostos a realizar os sonhos das crianças por meio da doação de presentes.
           </p>
