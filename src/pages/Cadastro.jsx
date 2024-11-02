466d73630f0f731393b7a4cf5597a34d4a6b5191
Hadriel Correia Gomes <gomeshadriel@gmail.com>
2024-11-02 17:42:21 -0300

    feat: logica de alteração de tabs e correções de design

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,174 +1,268 @@
 import {
-    Button,
-    ButtonGroup,
-    Checkbox,
-    InputLabel,
-    FormControl,
-    FilledInput,
-  }from "@mui/material";
-  import Link from "next/link";
-  import RedeemIcon from '@mui/icons-material/Redeem';
-  import React from 'react';
-  import UploadButton from "./upload";
-  import Contato from "./Contato";
-  
-  
-  
-  function Cadastro() {
-    const styles = {
-        
-      pagina :{
-        backgroundImage: 'url(img/BackgroundVermelho.png)',
-        height:'100vh',
-        display:'flex',
-        gap:'10vh',
-      },
+  Button,
+  ButtonGroup,
+  Checkbox,
+  InputLabel,
+  FormControl,
+  FilledInput,
+} from "@mui/material";
+import Link from "next/link";
+import React, { useState } from "react";
+import Contato from "./Contato";
 
-      main:{
-        display: 'flex',
-        justifyContent: 'space-around', 
-        alignItems: 'center',
-        gap: '20vh',
-        margin: '20px',
-      },
+function Cadastro() {
+  const styles = {
+    pagina: {
+      backgroundImage: "url(img/BackgroundVermelho.png)",
+      height: "100vh",
+      display: "flex",
+      gap: "10vh",
+    },
 
-      titulo: {
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center',
-      },
+    main: {
+      display: "flex",
+      justifyContent: "space-around",
+      alignItems: "center",
+      gap: "20vh",
+      margin: "20px",
+    },
 
-      tituloH1: {
-        fontFamily: 'Poppins',
-        fontSize: '36px',
-        fontWeight: '800',
-        lineHeight: '54px',
-      },  
+    titulo: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
 
-      Logo:{
-        display:"flex", 
-        gap:"2vh",
-      },
+    tituloH1: {
+      fontFamily: "Poppins",
+      fontSize: "36px",
+      fontWeight: "800",
+      lineHeight: "54px",
+    },
 
-      buttonPerson: {
-        background: "#007209",
-        width:'25vh',
-        height: '6vh',
-        textTransform: "none",
-        border:"none",
-      },
-  
-      form: {
-        height: '85vh',
-        width:'65vh',
-        marginLeft:'10%',
-        display:'flex',
-        flexDirection: 'column',
-        gap: "2vh",
-        background: "green",
-        padding:'2% 3%',
-      },
+    Logo: {
+      display: "flex",
+      gap: "2vh",
+    },
 
-      texto :{
-        background:'white',
-        height: "7vh",
-        fontSize: "small",
-      },
+    buttonPerson: (isActive) => ({
+      background: isActive ? "#0B7E14" : "#007209",
+      fontSize: "20px",
+      fontWeight: 600,
+      fontFamily: "poppins",
+      width: "100%",
+      height: "6vh",
+      textTransform: "none",
+      border: "none",
+      color: "#fff",
+      cursor: isActive ? "default" : "pointer",
+    }),
 
-      termos:{
-        display: "flex",
-        alignItems: "center",
-      },
+    form: {
+      height: "85vh",
+      width: "65vh",
+      marginLeft: "10%",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2vh",
+      background: "#007209",
+      padding: "2% 3%",
+    },
 
-      div:{
-        display: "flex",
-        flexDirection:'column',
-        height: "100%",
-        justifyContent: "space-between",
-        gap: "5%",
-      },
+    texto: {
+      background: "white",
+      height: "7vh",
+      fontSize: "small",
+    },
 
-      buttonRegistrar: {
-        background:'#E4BA46',
-        witdh:'auto',
-        height:'6vh',
-        display: 'block',
-        textTransform: "none",
-      },
+    termos: {
+      display: "flex",
+      alignItems: "center",
+    },
 
-      imagem: {
-        width:'30%',
-        height:'70%',
-      },
+    div: {
+      display: "flex",
+      flexDirection: "column",
+      height: "100%",
+      justifyContent: "space-between",
+      gap: "5%",
+    },
 
-      buttonVoltar:{
-        position:'absolute',
-        bottom:'10vh',
-        background: "#007209",
-        height:'7vh',
-        textTransform: "none",
-      },
-    };
-  
-  
-    return (
-        <>
-        <div style={styles.pagina}> 
-                    {/* O HTML vai dentro desta <div></div> */}    
-            <section style={styles.main}>
-                <div style={styles.form}>
-                    <div style={styles.titulo}>
-                        <h1 style={styles.tituloH1}>Cadastre-se</h1>
-                    </div>
+    buttonRegistrar: {
+      background: "#E4BA46",
+      width: "100%",
+      height: "6vh",
+      display: "block",
+      textTransform: "none",
+      fontSize: "20px",
+      fontWeight: 600,
+      fontFamily: "Poppins",
+    },
 
-                    <ButtonGroup style={styles.Logo} variant="text" aria-label="Basic button group">
-                        <Link href="" passHref><Button style={styles.buttonPerson} variant="contained">Escola</Button></Link>
-                        <Link href="" passHref><Button style={styles.buttonPerson} variant="contained">Padrinho</Button></Link>
-                    </ButtonGroup>
+    imagem: {
+      width: "30%",
+      height: "70%",
+    },
 
-                    <form style={styles.div}  action="processa_upload.php" method="post" encType="multipart/form-data">
+    buttonVoltar: {
+      position: "absolute",
+      bottom: "10vh",
+      background: "#007209",
+      height: "7vh",
+      textTransform: "none",
+    },
+    opcaoCadastro: {
+      gap: "8px",
+      display: "flex",
+      flexDirection: "column",
+    },
+  };
 
-                            <FormControl variant="filled">
-                                <InputLabel htmlFor="component-filled">Name</InputLabel>
-                                <FilledInput style={styles.texto} id="component-filled"/>
-                            </FormControl>
-                            <FormControl variant="filled">
-                                <InputLabel htmlFor="component-filled">INEP</InputLabel>
-                                <FilledInput style={styles.texto} id="component-filled"/>
-                            </FormControl>
-                            <FormControl variant="filled">
-                                <InputLabel htmlFor="component-filled">Email</InputLabel>
-                                <FilledInput style={styles.texto} id="component-filled"/>
-                            </FormControl>
-                            <FormControl variant="filled">
-                                <InputLabel htmlFor="component-filled">Senha</InputLabel>
-                                <FilledInput style={styles.texto} id="component-filled"/>
-                            </FormControl>
-                            <FormControl variant="filled">
-                                <InputLabel htmlFor="component-filled">Confirme a senha</InputLabel>
-                                <FilledInput style={styles.texto} id="component-filled"/>
-                            </FormControl> 
+  const [tipoCadastro, setTipoCadastro] = useState("padrinho");
 
-                    </form>
+  const selecionarPadrinho = () => setTipoCadastro("padrinho");
+  const selecionarEscola = () => setTipoCadastro("escola");
 
-                    <div style={styles.termos}>
-                        <Checkbox  defaultChecked color="white" />
-                        <p>Aceito os termos de utilização</p>
-                    </div>
-                    
-                    <Link href="" passHref><Button style={styles.buttonRegistrar} variant="contained" fullWidth >Registrar</Button></Link>
+  return (
+    <>
+      <div style={styles.pagina}>
+        <section style={styles.main}>
+          <div style={styles.form}>
+            <div style={styles.titulo}>
+              <h1 style={styles.tituloH1}>Cadastre-se</h1>
+            </div>
+            <div style={styles.opcaoCadastro}>
+              <h5>Fazer cadastro como:</h5>
+              <ButtonGroup style={styles.Logo} variant="text">
+                <Button
+                  onClick={selecionarEscola}
+                  disabled={tipoCadastro === "escola"}
+                  style={styles.buttonPerson(tipoCadastro === "escola")}
+                >
+                  Escola
+                </Button>
+                <Button
+                  onClick={selecionarPadrinho}
+                  disabled={tipoCadastro === "padrinho"}
+                  style={styles.buttonPerson(tipoCadastro === "padrinho")}
+                >
+                  Padrinho
+                </Button>
+              </ButtonGroup>
+            </div>
+
+            {tipoCadastro === "padrinho" ? (
+              <div>
+                <form
+                  style={styles.div}
+                  action="processa_upload.php"
+                  method="post"
+                  encType="multipart/form-data"
+                >
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">Nome</InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">CPF</InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">Email</InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">Senha</InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">
+                      Confirme a senha
+                    </InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                </form>
+
+                <div style={styles.termos}>
+                  <Checkbox defaultChecked />
+                  <p>Aceito os termos de utilização</p>
                 </div>
-                
-                <img style={styles.imagem} src="./img/papai.png" alt="imagem do papai noel" className="papai-noel"/>
-  
-                <Link  href="/" passHref> <Button style={styles.buttonVoltar} variant="contained">Voltar</Button></Link>
-  
-            </section>
-        </div>
-          <Contato/>
-          </>
-          
-      );
-    }
-  
-    export default Cadastro;
+
+                <Button
+                  style={styles.buttonRegistrar}
+                  variant="contained"
+                  fullWidth
+                >
+                  Registrar
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <form
+                  style={styles.div}
+                  action="processa_upload.php"
+                  method="post"
+                  encType="multipart/form-data"
+                >
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">
+                      Nome da Escola
+                    </InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">INEP</InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">Email</InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">Senha</InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                  <FormControl variant="filled">
+                    <InputLabel htmlFor="component-filled">
+                      Confirme a senha
+                    </InputLabel>
+                    <FilledInput style={styles.texto} id="component-filled" />
+                  </FormControl>
+                </form>
+
+                <div style={styles.termos}>
+                  <Checkbox defaultChecked />
+                  <p>Aceito os termos de utilização</p>
+                </div>
+
+                <Button
+                  style={styles.buttonRegistrar}
+                  variant="contained"
+                  fullWidth
+                >
+                  Registrar
+                </Button>
+              </div>
+            )}
+          </div>
+
+          <img
+            style={styles.imagem}
+            src="./img/papai.png"
+            alt="imagem do papai noel"
+            className="papai-noel"
+          />
+
+          <Link href="/" passHref>
+            <Button style={styles.buttonVoltar} variant="contained">
+              Voltar
+            </Button>
+          </Link>
+        </section>
+      </div>
+      <Contato />
+    </>
+  );
+}
+
+export default Cadastro;
